b70cb01d68608fcf5bdc74037cbe3793133f6156
Sayaki-M <sayaki3891.qwxzponm@gmail.com>
2020-08-30 18:21:35 +0900

    add 無装飾なアップロード+プレビュー画面

--- server/resources/views/save.blade.php
+++ server/resources/views/save.blade.php
@@ -20,6 +20,30 @@
     <body>
         <div class="content">
             これは画像保存ページです。
+            <input type = "file" id = "stonepicture" accept="image/*">
+            <img id = "preview">
+            <script type = "text/javascript">
+              function previewFile(file) {
+                // プレビュー画像を追加する要素
+                const preview = document.getElementById('preview');
+                // FileReaderオブジェクトを作成
+                const reader = new FileReader();
+                // URLとして読み込まれたときに実行する処理
+                reader.onload = function (e) {
+                  const imageUrl = e.target.result; // URLはevent.target.resultで呼び出せる
+                  preview.src = imageUrl; // URLをimg要素にセット
+                }
+                // いざファイルをURLとして読み込む
+                reader.readAsDataURL(file);
+              }
+
+              const fileInput = document.getElementById('stonepicture');
+              const handleFileSelect = () => {
+                const files = fileInput.files;
+                previewFile(files[0]);
+              }
+              fileInput.addEventListener('change', handleFileSelect);
+            </script>
         </div>
     </body>
 </html>
