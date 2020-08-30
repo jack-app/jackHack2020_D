016c0ba79f4cbf3e86ccdd6cd89491ac32e0b333
Sayaki-M <sayaki3891.qwxzponm@gmail.com>
2020-08-30 18:47:42 +0900

    add サンプル画像 change 無装飾なステータス画面

--- server/resources/views/confirm.blade.php
+++ server/resources/views/confirm.blade.php
@@ -20,6 +20,29 @@
     <body>
         <div class="content">
             これはステータス確認ページです。
+            <div class="picture">
+              <img src="./DSC_0923.png" width="128" height="128" alt="石の画像">
+            </div>
+            <div class= "status">
+              <div class="attack">
+                ここが攻撃力
+              </div>
+              <div class="defence">
+                ここが防御力
+              </div>
+              <div class="HP">
+                ここが体力
+              </div>
+              <div class="stonecolor">
+                ここが色
+              </div>
+            </div>
+            <p>
+              <input type="button" value="戦う">
+            </p>
+            <p>
+              <input type="button" value="石を選びなおす">
+            </p>
         </div>
     </body>
 </html>
