b97a620fbe121f0cda0145c6140818f5d7110945
yunine16 <yuchin616@icloud.com>
2020-08-31 14:05:15 +0900

    ConfirmController追記

--- server/app/Http/Controllers/ConfirmController.php
+++ server/app/Http/Controllers/ConfirmController.php
@@ -5,33 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Stones;
+use App\User;
 
 class ConfirmController extends Controller
 {
     public function index() {
-        return view('confirm');
-    }
-
-    public function saveStoneData(Request $request) {
-        // $validator = Validator::make($request->all(),[
-        //         'class_no'    => 'required|nullable|string|max:10',
-        //         'grade_id'    => 'required|nullable|integer|digits_between:1,11',
-        //         'category_id' => 'required|integer|digits_between:1,20',
-        // ]);
-
-        $stone = new Stones;
-        $post_data = $request->all();
-        $post_data['user_id'] = 1;
-
-        $stone->user_id = $post_data['user_id'];
-        $stone->stone_name = $post_data['name'];
-        $stone->hp = $post_data['hp'];
-        $stone->attack = $post_data['attack'];
-        $stone->defence = $post_data['defence'];
-        $stone->color = $post_data['color'];
-
-        $stone->save();
-
-        return view('fight');
+        $user_stone = User::find(1)->stones;
+        $enemy_stone = User::find(2)->stones;
+        return view('confirm',compact('enemy_stone','user_stone'));
     }
 }
--- server/resources/views/save.blade.php
+++ server/resources/views/save.blade.php
@@ -8,7 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        
         <!-- Styles -->
         <style>
         .main{
